dabc9c3fd7f48db57d2ff9abd91e6c5e2af1c606
BLEAX <rlawjdgns135@naver.com>
2024-03-22 10:15:42 +0900

    Update index.js

--- index/index.js
+++ index/index.js
@@ -1,3 +1,10 @@
+//express
+const express = require('express');
+const expressApp = express();
+const port = 80;
+
+//firebase
+const app = initializeApp(firebaseConfig);
 const firebaseConfig = {
   apiKey: "AIzaSyC4JdgfKdIAeTh5QfzhoMF-dDWPfQewbKE",
   authDomain: "capstonedesign-eb13f.firebaseapp.com",
@@ -7,4 +14,17 @@ const firebaseConfig = {
   appId: "1:1054972036829:web:e25dcd0a7e6159d9177d34",
 };
 
-const app = initializeApp(firebaseConfig);
+
+expressApp.get('/', (req, res) => {
+  res.send('');
+});
+
+/*
+expressApp.post('/', (req, res) => {
+  res.send()
+});
+*/
+
+
+expressApp.listen(port, () => {
+});
