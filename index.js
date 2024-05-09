b8907ba8cacbff17e8c54a033ce21b8acc6db098
BLEAX <rlawjdgns135@naver.com>
2024-05-10 01:12:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const path = require("path");
 const app = express();
 const port = 80;
 
+//XSS방지용 모듈
+const sanitizeHTML = require("sanitize-html");
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
